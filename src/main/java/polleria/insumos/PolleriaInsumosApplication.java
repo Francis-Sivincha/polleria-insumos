3658f710bf19fe0d69651c6bfa7563151dbd5a53
Francis-Sivincha <matioli_20_3@hotmail.com>
2025-09-17 12:26:58 -0500

    primer commit

---
+++ src/main/java/polleria/insumos/PolleriaInsumosApplication.java
@@ -0,0 +1,13 @@
+package polleria.insumos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolleriaInsumosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolleriaInsumosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/polleria/insumos/PolleriaInsumosApplicationTests.java
@@ -0,0 +1,13 @@
+package polleria.insumos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolleriaInsumosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
